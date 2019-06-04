0d7d50a0ced4f1452548fad63a4eaef585f68c8a
James DeWaard <dewaard@f018981018d4.ant.amazon.com>
2019-06-04 10:08:53 -0500

    updated the cfn templates

---
+++ 01-container-basics/101-start-here/utilities/entrypoint.sh
@@ -0,0 +1,43 @@
+# Configure AWS CLI
+availability_zone=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone)
+export AWS_DEFAULT_REGION=${availability_zone%?}
+
+# Lab-specific configuration
+export AWS_MASTER_STACK="bryanvh-eks-vpc"
+
+# EKS-specific variables from CloudFormation
+export EKS_VPC_ID=$(aws cloudformation describe-stacks --stack-name $AWS_MASTER_STACK | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="VpcId")|.OutputValue')
+export EKS_SUBNET_IDS=$(aws cloudformation describe-stacks --stack-name $AWS_MASTER_STACK | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="SubnetIds")|.OutputValue')
+export EKS_SECURITY_GROUPS=$(aws cloudformation describe-stacks --stack-name $AWS_MASTER_STACK | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="SecurityGroups")|.OutputValue')
+export EKS_SERVICE_ROLE=$(aws cloudformation describe-stacks --stack-name $AWS_MASTER_STACK | jq -r '.Stacks[0].Outputs[]|select(.OutputKey=="EksServiceRoleArn")|.OutputValue')
+
+# EKS-Optimized AMI
+if [ "$AWS_DEFAULT_REGION" == "us-east-1" ]; then
+  export EKS_WORKER_AMI=ami-0abcb9f9190e867ab
+elif [ "$AWS_DEFAULT_REGION" == "us-west-2" ]; then
+  export EKS_WORKER_AMI=ami-0923e4b35a30a5f53
+fi
+echo "EKS_NODE_AMI"=$EKS_WORKER_AMI >> /etc/environment
+
+# Persist lab variables
+echo "AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION" >> /etc/environment
+echo "AWS_MASTER_STACK=$AWS_MASTER_STACK" >> /etc/environment
+
+# Persist EKS variables
+echo "EKS_VPC_ID=$EKS_VPC_ID" >> /etc/environment
+echo "EKS_SUBNET_IDS=$EKS_SUBNET_IDS" >> /etc/environment
+echo "EKS_SECURITY_GROUPS=$EKS_SECURITY_GROUPS" >> /etc/environment
+echo "EKS_SERVICE_ROLE=$EKS_SERVICE_ROLE" >> /etc/environment
+
+
+# Create SSH key
+# ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
+
+# # Create EC2 Keypair
+# aws ec2 create-key-pair --key-name ${EKS_CLUSTER_NAME} --query 'KeyMaterial' --output text > $HOME/.ssh/k8s-workshop.pem
+# chmod 0400 $HOME/.ssh/k8s-workshop.pem
+#
+# if [ ! -d "aws-workshop-for-kubernetes/" ]; then
+#   # Download lab Repository
+#   git clone https://github.com/dewjam/aws-workshop-for-kubernetes.git
+# fi
